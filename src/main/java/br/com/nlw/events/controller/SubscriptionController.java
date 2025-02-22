ea77d7daf25f0231067c6502ec397e2078e9a94c
Eduardo Serafim <eduserafim7@gmail.com>
2025-02-22 19:59:00 -0300

    Métodos doSubscription Controller , exceptions, e DTO

---
+++ src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -0,0 +1,36 @@
+package br.com.nlw.events.controller;
+
+import br.com.nlw.events.DTO.ErrorMessage;
+import br.com.nlw.events.exceptions.EventNotFoundException;
+import br.com.nlw.events.exceptions.SubscriptionConflictException;
+import br.com.nlw.events.model.Subscription;
+import br.com.nlw.events.model.User;
+import br.com.nlw.events.service.SubscriptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/subscription")
+public class SubscriptionController {
+
+    @Autowired
+    private SubscriptionService subscriptionService;
+
+    @PostMapping("/{prettyName}")
+    public ResponseEntity<?> createSubscription(@PathVariable String prettyName, @RequestBody User subscriber){
+        try {
+            Subscription res = subscriptionService.createNewSubscription(prettyName, subscriber);
+            if (res != null){
+                return ResponseEntity.ok().body(res);
+            }
+        }catch (EventNotFoundException ex){
+            return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
+        }catch (SubscriptionConflictException ex){
+            return ResponseEntity.status(409).body(new ErrorMessage(ex.getMessage()));
+        }
+        return ResponseEntity.badRequest().build();
+
+
+    }
+}
---
+++ src/main/java/br/com/nlw/events/service/SubscriptionService.java
@@ -0,0 +1,47 @@
+package br.com.nlw.events.service;
+
+import br.com.nlw.events.exceptions.EventNotFoundException;
+import br.com.nlw.events.exceptions.SubscriptionConflictException;
+import br.com.nlw.events.model.Event;
+import br.com.nlw.events.model.Subscription;
+import br.com.nlw.events.model.User;
+import br.com.nlw.events.repo.EventRepo;
+import br.com.nlw.events.repo.SubscriptionRepo;
+import br.com.nlw.events.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+
+    @Autowired
+    private EventRepo eventRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private SubscriptionRepo subscriptionRepo;
+
+    public Subscription createNewSubscription(String eventName, User user){
+        Event evt = eventRepo.findByPrettyName(eventName);
+        if(evt == null){
+            throw new EventNotFoundException("Evento " + eventName + " não existe");
+        }
+        User userRec = userRepo.findByEmail(user.getEmail());
+        if(userRec == null){
+            userRec = userRepo.save(user);
+        }
+        Subscription subs = new Subscription();
+        subs.setEvent(evt);
+        subs.setSubscriber(userRec);
+
+        Subscription tmpSub = subscriptionRepo.findByEventAndSubscriber(evt, userRec);
+        if(tmpSub != null){
+            throw new SubscriptionConflictException("Já existe inscrição para o usuário " + userRec.getName() + " no evento " + evt.getTitle());
+        }
+
+        return subscriptionRepo.save(subs);
+    }
+
+}
